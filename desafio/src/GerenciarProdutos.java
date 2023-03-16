4c85d3f3a03b79653e0ecceab2b8020e2c1c6b3e
Eric Lodi Luz <101107093+EricLuz7@users.noreply.github.com>
2023-03-16 17:36:50 -0300

    Add files via upload

---
+++ desafio/src/GerenciarProdutos.java
@@ -0,0 +1,114 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
+public class GerenciarProdutos {
+    public List<Produto> produtos = new ArrayList<>();
+    public Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        GerenciarProdutos gp = new GerenciarProdutos();
+        int opcao = 0;
+        do{
+            System.out.println(" Gerenciamento de produtos");
+            System.out.println(" ====================================");
+            System.out.println(" [ 1. Cadastrar produto             ]");
+            System.out.println(" [ 2. Consultar produtos            ]");
+            System.out.println(" [ 3. Registrar entrada             ]");
+            System.out.println(" [ 4. Registrar saida               ]");
+            System.out.println(" [ 5. Consultar valor em estoque    ]");
+            System.out.println(" [ 9. Sair                          ]");
+            opcao = parseInt(sc.nextLine());
+            switch (opcao){
+                case 1:
+                    gp.execCadastrar();
+                    break;
+                case 2:
+                    gp.execConsultar();
+                    break;
+                case 3:
+                    gp.execDarEntrada();
+                    break;
+                case 4:
+                    gp.execDarSaida();
+                    break;
+                case 5:
+                    gp.execConsultarValorEstoque();
+                    break;
+                case 9:
+                    System.out.println("Fim do programa");
+                    break;
+                default:
+                    System.out.println("Opcao invalida");
+            }
+        }while(opcao != 9);
+    }
+    public void execCadastrar(){
+        Produto produto = new Produto();
+        double porcentagemPrecoCusto;
+        System.out.println("Cadastrando produto....");
+        System.out.println("Digite o codigo do produto: ");
+        produto.setCodigo(parseInt(sc.nextLine()));
+        System.out.println("Digite o nome do produto: ");
+        produto.setNome(sc.nextLine());
+        System.out.println("Digite o preco de custo do produto: ");
+        produto.setPrecoCusto(Double.parseDouble(sc.nextLine()));
+        System.out.println("O preco de venda sera quantos % do preco de custo?");
+        porcentagemPrecoCusto = (Double.parseDouble(sc.nextLine()));
+        produto.preco(porcentagemPrecoCusto);
+        produtos.add(produto);
+        System.out.println("Produto cadastrado");
+    }
+    public void execConsultar(){
+        System.out.println("Exibindo todos os produtos cadastrados");
+        for(Produto p : produtos){
+                System.out.println(p.toString());
+                System.out.println();
+        }
+    }
+    public void execDarEntrada(){
+        int cod;
+        int quantidade;
+        System.out.println("Digite o codigo do produto: ");
+        cod = Integer.parseInt(sc.nextLine());
+        for(Produto p: produtos){
+            if(p.getCodigo()== cod){
+                System.out.println("Deseja adicionar quantas unidades?");
+                quantidade = Integer.parseInt(sc.nextLine());
+                p.darEntrada(quantidade);
+                System.out.println("Adicionado com sucesso");
+                return;
+            }
+        }
+        System.out.println("Produto não cadastrado");
+    }
+    public void execDarSaida(){
+        int cod;
+        int quantidade;
+        System.out.println("Digite o codigo do produto: ");
+        cod = Integer.parseInt(sc.nextLine());
+        for(Produto p: produtos){
+            if(p.getCodigo()== cod){
+                System.out.println("Deseja retirar quantas unidades?");
+                quantidade = Integer.parseInt(sc.nextLine());
+                if(quantidade > 0){
+                    p.darSaida(quantidade);
+                    System.out.println("Retirado com sucesso");
+                    return;
+                }
+            }
+        System.out.println("Produto não cadastrado");
+        }
+    }
+    public void execConsultarValorEstoque(){
+        double valorTotal;
+        for(Produto p: produtos){
+             valorTotal = p.getEstoque() * p.getPrecoVenda();
+            System.out.println(valorTotal);
+            return;
+        }
+    }
+}
+
---
+++ desafio/src/Produto.java
@@ -0,0 +1,71 @@
+public class Produto {
+    private int codigo;
+    private String nome;
+    private double precoVenda;
+    private double precoCusto;
+    private int estoque;
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+
+    public double getPrecoCusto() {
+        return precoCusto;
+    }
+
+    public void setPrecoCusto(double precoCusto) {
+        this.precoCusto = precoCusto;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPrecoVenda() {
+        return precoVenda;
+    }
+
+    public void setPrecoVenda(double precoVenda) {
+        this.precoVenda = precoVenda;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "codigo=" + codigo +
+                ", nome='" + nome + '\'' +
+                ", precoVenda=" + precoVenda +
+                '}';
+    }
+
+    public void preco(double porcentagemPrecoCusto){
+        precoVenda = precoCusto * (porcentagemPrecoCusto/100);
+    }
+    public void darEntrada(int quantidade){
+        estoque += quantidade;
+    }
+
+    public boolean darSaida(int quantidade){
+        if(quantidade<=estoque){
+            estoque -= quantidade;
+            return true;
+        }
+        return false;
+    }
+}
